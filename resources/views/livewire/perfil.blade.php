623ca8984d7794e2013bf38551c952b4f0109c29
ynalbizz <cdavic13@gmail.com>
2026-05-13 16:52:34 -0300

    teste1

--- resources/views/livewire/perfil.blade.php
+++ resources/views/livewire/perfil.blade.php
@@ -35,7 +35,9 @@
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
                     <script>
                         document.addEventListener("DOMContentLoaded", function() {
-                            let pid = "{{$participante->pid}}";
+                            let pid = "{{ $this->userAtual->pid ?? '' }}";
+                            if (!pid) return;
+
                             new QRCode(document.getElementById("qrcode"),{
                                 text: pid,
                                 width: 150,
