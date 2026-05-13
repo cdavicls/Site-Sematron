1ed4f8d0a2956903b6b0a35343274e520c8fe286
Davi Carvalho <cdavic13@gmail.com>
2026-05-13 11:13:12 -0300

    lista de pessoinhas atualizada

--- app/Http/Controllers/admController.php
+++ app/Http/Controllers/admController.php
@@ -21,15 +21,18 @@ class admController extends Controller
                                         userinfos.nusp,
                                         userinfos.tel,
                                         sales.code,
+                                        userdata.camiseta as camiseta,
                                         sales.status as sales_status,
                                         userdata.status as status_usuario,
-                                        events.name as viagem_usuario,
+                                        viagem.name as viagem_usuario,
+                                        minicurso.name as minicurso,
                                         pack.nome as pack_usuario
                                     FROM userdata
                                     INNER JOIN userinfos ON userdata.uid = userinfos.uid
                                     INNER JOIN sales ON userdata.pid = sales.pid
                                     INNER JOIN pack ON userdata.pack_id = pack.id
-                                    LEFT JOIN events ON userdata.viagem = events.eid
+                                    LEFT JOIN events AS viagem ON userdata.viagem = viagem.eid
+                                    LEFT JOIN events AS minicurso ON userdata.minicurso = minicurso.eid
                                     WHERE userdata.sid = 22');
     
         return view('adm_list_insc',['participantes' => $resultados]);
